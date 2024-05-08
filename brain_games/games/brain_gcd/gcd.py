76493d43335bea189e63d3b6edf32edec9db5cd5
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-05-08 15:24:56 +0300

    Step 7 (game gcd)

---
+++ brain_games/games/brain_gcd/gcd.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+
+RAND_NUMBER_MIN = 1
+RAND_NUMBER_MAX = 100
+
+questions = 'Find the greatest common divisor of given numbers.'
+
+def number_in_questions():
+    rand_num_1 = random.randint(RAND_NUMBER_MIN, RAND_NUMBER_MAX)
+    rand_num_2 = random.randint(RAND_NUMBER_MIN, RAND_NUMBER_MAX)
+    number = f'{rand_num_1} {rand_num_2}'
+    correct_answer = str(math.gcd(rand_num_1, rand_num_2))
+    return number, correct_answer
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from brain_games.core import game_logic 
+from brain_games.games.brain_gcd import gcd
+
+
+def main():
+    game_logic(gcd)
+    
+    
+if __name__ == '__main__':
+    main()
+    
