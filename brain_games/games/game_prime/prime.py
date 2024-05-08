3d5659926e65f05bc66933b317614d1935b93b4b
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-05-08 18:56:27 +0300

    Step 9 (game prime)

---
+++ brain_games/games/game_prime/prime.py
@@ -0,0 +1,22 @@
+from random import randint
+
+RAND_NUMBER_MIN = 1
+RAND_NUMBER_MAX = 100
+
+questions = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+def number_in_questions():
+    number = randint(RAND_NUMBER_MIN, RAND_NUMBER_MAX)
+    if answer_in_questions(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return number, correct_answer
+
+def answer_in_questions(number):
+    if number > 1:
+        for n in range(2, number):
+            if (number % n) == 0:
+                return False
+        return True
+    else:
+        return False
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from brain_games.core import game_logic 
+from brain_games.games.game_prime import prime
+
+
+def main():
+    game_logic(prime)
+    
+    
+if __name__ == '__main__':
+    main()
+   
