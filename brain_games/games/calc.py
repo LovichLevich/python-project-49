0d2edce4365044b9e98415bf10160bff651a1882
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2024-05-15 11:18:36 +0300

    correction of Anton Olenev's comments #2.1

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -11,8 +11,7 @@ def create_numbers_in_question():
     rand_num_1 = random.randint(RAND_NUMBER_MIDDLE, RAND_NUMBER_MAX)
     rand_num_2 = random.randint(RAND_NUMBER_MIN, RAND_NUMBER_MIDDLE)
     operator = random.choice('+-*')
-    game_question = f'{rand_num_1} {operator} {rand_num_2}' #The variable is named "number" 
-    #because of its further implementation in the def game_generate(game) function. 
+    game_question = f'{rand_num_1} {operator} {rand_num_2}'
     correct_answer = str(evaluate(rand_num_1, operator, rand_num_2))
     return game_question, correct_answer
 
